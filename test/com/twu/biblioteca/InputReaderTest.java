50f99121158f9374aaff8c3f8cf7b1f054a7faf3
Lucerne <lucerne_liu@foxmail.com>
2018-04-27 02:40:26 +0800

    test:add test "should_return_input_option_when_is_right"

--- test/com/twu/biblioteca/InputReaderTest.java
+++ test/com/twu/biblioteca/InputReaderTest.java
@@ -3,6 +3,12 @@ package com.twu.biblioteca;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.Field;
+import java.util.Scanner;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class InputReaderTest {
     private InputReader reader;
 
@@ -10,6 +16,17 @@ public class InputReaderTest {
     public void setUp(){
         reader = new InputReader();
     }
-    
 
+    private void setInputStream(String input) throws NoSuchFieldException, IllegalAccessException {
+        Field scannerField = reader.getClass().getDeclaredField("scanner");
+        Scanner scannerWithMockStream = new Scanner(new ByteArrayInputStream(input.getBytes()));
+        scannerField.setAccessible(true);
+        scannerField.set(reader, scannerWithMockStream);
+    }
+    
+    @Test
+    public void should_return_input_option_when_is_right() throws NoSuchFieldException, IllegalAccessException {
+        setInputStream("1");
+        assertThat(reader.readOption()).isEqualTo("1");
+    }
 }
