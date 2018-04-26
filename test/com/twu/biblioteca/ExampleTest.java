d9e52ccbfb6857fb774829284dae362eedadba6e
Lucerne <lucerne_liu@foxmail.com>
2018-04-26 19:21:19 +0800

    test:add test "should_print_welcome_message_when_start_app"

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,34 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import  static org.fest.assertions.api.Assertions.assertThat;
 
 public class ExampleTest {
+    private BibliotecaApp bibliotecaApp;
+    private ByteArrayOutputStream outputContent;
+
+    @Before
+    public void setUp() {
+        bibliotecaApp = new BibliotecaApp();
+        outputContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputContent));
+    }
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void should_print_welcome_message_when_start_app(){
+        bibliotecaApp.start();
+        assertThat(systemOut().startsWith("Welcome to use the Biblioteca!")).isTrue();
+    }
+
+    private String systemOut(){
+        return outputContent.toString();
     }
 }
