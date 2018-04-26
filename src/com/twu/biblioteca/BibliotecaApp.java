d5b82d180b17baee8f999518acf9fa5569e496b8
Lucerne <lucerne_liu@foxmail.com>
2018-04-26 19:53:14 +0800

    feat:implement test "should_print_main_menu_after_welcome_message"

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -10,4 +10,7 @@ public class BibliotecaApp {
         System.out.println("Welcome to use the Biblioteca!");
     }
 
+    public void printMainMenu() {
+        System.out.print("1. List Books\n2. Quit\nPlease enter your choice(1～2):");
+    }
 }
